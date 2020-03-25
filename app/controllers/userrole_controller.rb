3c985d6eed25fb6fb1fee701e7ed6fdd32e805ff
udeogu ifechuwku <ifebrand6@gmail.com>
2020-03-25 20:42:21 +0100

    test user role with views and controllers

---
+++ app/controllers/userrole_controller.rb
@@ -0,0 +1,8 @@
+class UserroleController < ApplicationController
+  # load_and_authorize_resource
+  def index
+  end
+
+  def test
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  get 'userrole/index'
+  get 'userrole/test'
   devise_for :user
   get "/test", to: "home#test"
-
 end
