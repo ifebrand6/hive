9e3dd460fea35d7f51ee6823a841c96b1b200711
udeogu ifechuwku <ifebrand6@gmail.com>
2020-03-25 19:54:37 +0100

    added cancan gem to handle userLevelAuthorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_092159) do
+ActiveRecord::Schema.define(version: 2020_03_25_163238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2020_03_25_092159) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "superadmin_role", default: false
+    t.boolean "admin_role", default: false
+    t.boolean "customer_role", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
