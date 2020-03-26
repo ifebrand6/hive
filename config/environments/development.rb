725196cc9cc1a890592c00d685db3d8cc8a9936c
udeogu ifechuwku <ifebrand6@gmail.com>
2020-03-26 06:38:13 +0100

    fixed nameError issue for recovery pwd mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.peform_deliveries = true
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.raise_delivery_errors = true
 
