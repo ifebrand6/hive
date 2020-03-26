f281e20fedf33aa2ca9339ce9ec1697a4261a691
udeogu ifechuwku <ifebrand6@gmail.com>
2020-03-26 05:18:14 +0100

    user mail welcome address

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+    after_create :send_admin_mail
+    
+    def send_admin_mail
+      UserMailer.send_welcome_email(self).deliver_later
+    end
 end
