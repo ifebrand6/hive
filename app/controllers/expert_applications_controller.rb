f3b982bcaae531cce055b1e6692a2d427a74e157
udeogu ifechuwku <ifebrand6@gmail.com>
2020-03-27 23:10:40 +0100

    added suggested expert skills column

--- app/controllers/expert_applications_controller.rb
+++ app/controllers/expert_applications_controller.rb
@@ -4,15 +4,14 @@ class ExpertApplicationsController < ApplicationController
   def index
     @expert_specialization_list = TalentType.all
   end
+
   def new
     @expert_specialization_list = TalentType.all
     @expert_application = ExpertApplication.new
-    # @expert_application.talent_type = TalentType.new
   end
+
   def create
     @expert_specialization_list = TalentType.all
-    # @expert_specialization_list.new(talent_type_params)
-    # @expert_application.talent_type.save
     @expert_application = ExpertApplication.new(expert_application_params)
     if @expert_application.save
       redirect_to action: "index", flash: "APPLICATION SUBMITTED SUCCESSFULLY"
@@ -20,9 +19,11 @@ class ExpertApplicationsController < ApplicationController
       render :new
     end
   end
+
+
     private
     def expert_application_params
-      params.require(:expert_application).permit(:firstName,:lastName,:email, :phoneNumber, :contactAddress,:shortBio, :talent_type_id)
+      params.require(:expert_application).permit(:firstName,:lastName,:email, :phoneNumber, :contactAddress,:shortBio, :talent_type_id, :suggested_skill)
     end
     def talent_type_params
       params.require(:talent_type).permit(:expert_specialization)
