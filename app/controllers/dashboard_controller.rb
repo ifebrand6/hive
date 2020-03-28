8a6d10427b30c01c6fc5ba5ed855e90c87117e74
udeogu ifechuwku <ifebrand6@gmail.com>
2020-03-28 22:31:06 +0100

    added onboarding fxn and reject application fxn

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,22 @@
 class DashboardController < ApplicationController
+  skip_before_action :authenticate_user!, :except => []
+ #before_action :talent_types
+
   def index
   end
 
   def application
+    @expert_application_list = ExpertApplication.all.pending_application
   end
 
   def talents
+    @expert_application_list = ExpertApplication.all.onboard_expert
   end
+
+  private
+     def talent_types
+      @talent_types = TalentType.all
+     end
 end
+
+
