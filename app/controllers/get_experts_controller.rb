a31148816ffb070d51f4b83bd4a403779f597854
udeogu ifechuwku <ifebrand6@gmail.com>
2020-03-29 02:42:38 +0100

    created request model

--- app/controllers/get_experts_controller.rb
+++ app/controllers/get_experts_controller.rb
@@ -2,5 +2,17 @@ class GetExpertsController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index]
 
   def index
+    if (current_user.customer_role === true)
+      @request = Request.new(user_id: current_user.id)
+      @request.save! 
+    else
+      redirect_to root_path 
+    end
   end
+
+  private
+    def request_params
+      params.require(:request).permit(:user_id)
+    end
+    
 end
