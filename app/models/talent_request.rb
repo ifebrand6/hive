ebf695c8dbdf5b79d9b6b79a5494eb18504253b7
udeogu ifechuwku <ifebrand6@gmail.com>
2020-03-31 15:05:56 +0100

    delete guest user when user sign in

--- app/models/talent_request.rb
+++ app/models/talent_request.rb
@@ -1,5 +1,6 @@
 class TalentRequest < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   belongs_to :request
   belongs_to :talent_type
+  accepts_nested_attributes_for :request,  allow_destroy: true
 end
