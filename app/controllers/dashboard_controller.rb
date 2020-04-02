7a623c4be98ebf483c1063b5416894afcc4e6c29
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-02 18:11:35 +0100

    customer request controller actions and views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
-  skip_before_action :authenticate_user!, :except => []
+  #skip_before_action :authenticate_user!, :except => []
 
   def index
   end
@@ -11,10 +11,23 @@ class DashboardController < ApplicationController
   def talents
     @expert_application_list = ExpertApplication.all.onboard_expert
   end
+  def customers_requests
+    @customers = User.all.where(customer_role: true)
+  end
+  def customer_requests
+    @user = User.find(params[:id])
+    @requests = @user.requests
+  end
+  def customer_talent_requests
+    @request = Request.find(params[:id])
+    @talent_requests =  @request.talent_requests
+  end
+  
 
+  
   private
      def talent_types
-      @talent_types = TalentType.all
+      @talent_types = TalentType.all.find
      end
 end
 
--- app/models/talent_request.rb
+++ app/models/talent_request.rb
@@ -2,5 +2,6 @@ class TalentRequest < ApplicationRecord
   # belongs_to :user
   belongs_to :request
   belongs_to :talent_type
+  belongs_to :user
   accepts_nested_attributes_for :request,  allow_destroy: true
 end
