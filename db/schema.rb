7ba45010288a76f3a1a91b852845d197117a66ca
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-02 18:07:05 +0100

    + email & ph-no to request table, so unknown

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_151747) do
+ActiveRecord::Schema.define(version: 2020_03_31_091024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2020_03_30_151747) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "phoneNumber"
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
