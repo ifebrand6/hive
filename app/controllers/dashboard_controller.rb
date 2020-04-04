846688739e68ae3cdd57714874d2049c0044db18
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-04 23:32:58 +0100

    finalized a request

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -22,13 +22,27 @@ class DashboardController < ApplicationController
     @request = Request.find(params[:id])
     @talent_requests =  @request.talent_requests
   end
-  
-
+  def talent_assignment
+    @talent_request = TalentRequest.find(params[:id]) #use to get the TalentRequest and populated to fill in the final request params
+    @talents = ExpertApplication.all.where(talent_type: @talent_request.talent_type_id)
+    @final_request = FinalizedRequest.new
+    @final_request.talent_assignments.build
+  end
+  def finalize_user_request
+      @final_request = FinalizedRequest.new(final_params)
+      if @final_request.save
+      flash[:notice] = "Talent Assign successfully"
+      redirect_to root_path
+    else
+      render :talent_assignment
+    end
+  end
   
   private
-     def talent_types
-      @talent_types = TalentType.all.find
-     end
+     
+     def final_params
+      params.require(:finalized_request).permit(:user_id, :request_id, talent_assignments_attributes: [:id,:engaged_date, :start_date,:expert_application_id, :finalized_request, :_destroy])
+    end
 end
 
 
