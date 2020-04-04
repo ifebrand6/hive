b408948ec668de882e9c10e8fed3b42e2a9f621c
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-04 23:32:34 +0100

    finalized request

--- app/models/finalized_request.rb
+++ app/models/finalized_request.rb
@@ -1,4 +1,12 @@
 class FinalizedRequest < ApplicationRecord
   belongs_to :user
   belongs_to :request
+  has_one :talent_assignment
+
+  accepts_nested_attributes_for :talent_assignment, reject_if: :reject_talent_assignment,  allow_destroy: true
+
+  def reject_talent_assignment
+    attributes['start_date'].blank?
+  end
+  
 end
