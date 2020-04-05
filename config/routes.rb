d6e23948b6df18754dc00c9384ff3dce89fb32ff
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-05 18:53:15 +0100

    added routes for contact and partnership link

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   post "dashboard/finalize_user_request", to: "dashboard#finalize_user_request"
   get 'onboard/:id' => 'expert_applications#accept_application', :as => 'onboard'
   #get '/apply', to: " expert_application#apply" #rename route so site/career/appy
-  resources :messages
   resources :expert_applications, path: 'application'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -20,4 +19,7 @@ Rails.application.routes.draw do
   get 'userrole/test'
   devise_for :user
   get "/test", to: "home#test"
+  get '/become-partner', to: "home#become_a_partner"
+  get '/contact', to: "home#contact"
+
 end
