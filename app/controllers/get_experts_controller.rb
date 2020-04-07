8cb859c8674509363030b734dc21151fe6bdaa58
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-07 15:01:35 +0100

    clean up get expert code

--- app/controllers/get_experts_controller.rb
+++ app/controllers/get_experts_controller.rb
@@ -1,41 +1,24 @@
 class GetExpertsController < ApplicationController
   def index
-    
     @expert_specialization_list = TalentType.all
-    @request = Request.new
-    @request.build_talent_request
-    # @request = Request.new(user_id: current_or_guest_user.id)
-    # @request.save! 
-    # @talent_requests = @request.talent_requests.build
-    #@talent_requests = TalentRequest.new
-    # request = @talent_requests.build_request
+    @requests = Request.new
+    @requests.build_talent_request
   end
   def create
     @expert_specialization_list = TalentType.all
-    @request = Request.new(request_params)
-    if @request.save
+    @requests = Request.new(request_and_talent_request_params)
+    if @requests.save
+      # send mail to user and admin
       flash[:notice] = "You request has been recieved."
       redirect_to root_path, notice: 'request has been recieved.'
     else
       render :index
     end
-    # @talent_requests = TalentRequest.new(talent_request_params)
-    #@talent_requests = TalentRequest.new(talent_request_params)
-    # if @talent_requests.save!
-    #   redirect_to root_path, notice: 'request has been recieved.'
-    # else
-    #   render :index
-    # end
   end
   
 
   private
-    def request_params
+    def request_and_talent_request_params
       params.require(:request).permit(:user_id,:email,:phoneNumber,talent_request_attributes: [:user_id,:unit_price,:expected_contract_duration,:expected_start_date,:quantity,:talent_type_id])
     end
-    def talent_request_params
-      params.require(:request_params).permit(:user_id,:email,:phoneNumber,talent_request_attributes: [:unit_price,:expected_contract_duration,:expected_start_date,:quantity,:talent_type_id])
-    end
-    
-    
 end
--- app/models/talent_request.rb
+++ app/models/talent_request.rb
@@ -1,5 +1,4 @@
 class TalentRequest < ApplicationRecord
-  # belongs_to :user
   belongs_to :request
   belongs_to :talent_type
   belongs_to :user
