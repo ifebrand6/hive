e8b2f81efa67b86fe0f6766130f02919993c7b8f
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-09 14:41:25 +0100

    remove comfy gem

--- config/routes.rb
+++ config/routes.rb
@@ -12,14 +12,15 @@ Rails.application.routes.draw do
   get 'onboard/:id' => 'expert_applications#accept_application', :as => 'onboard'
   #get '/apply', to: " expert_application#apply" #rename route so site/career/appy
   resources :expert_applications, path: 'application'
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
   get 'userrole/index'
   get 'userrole/test'
   devise_for :user
   get "/test", to: "home#test"
   get '/become-partner', to: "home#become_a_partner"
+  get '/home', to: "home#index"
   get '/contact', to: "home#contact"
   post "home/create", to: "home#create"
 end
