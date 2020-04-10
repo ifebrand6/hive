e39778698872aac10e5e63d318a63c44b5bf6b60
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-10 16:02:50 +0100

    clean up migration files

--- db/migrate/20200409150410_add_description_to_posts.blogit.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from blogit (originally 20150618184415)
-class AddDescriptionToPosts < ActiveRecord::Migration[4.2]
-  def change
-    add_column :blogit_posts, :description, :text
-  end
-end
