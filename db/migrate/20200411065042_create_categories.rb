3c8b66ed2c2a40f64895af67dfe6178ba8741613
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-11 09:19:17 +0100

    ran post migrations

---
+++ db/migrate/20200411065042_create_categories.rb
@@ -0,0 +1,9 @@
+class CreateCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :categories do |t|
+      t.string :name, limit: 50
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_133209) do
+ActiveRecord::Schema.define(version: 2020_04_11_070138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", limit: 50
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comfy_cms_blocks", force: :cascade do |t|
     t.string "identifier", null: false
@@ -167,6 +173,17 @@ ActiveRecord::Schema.define(version: 2020_04_08_133209) do
     t.index ["user_id"], name: "index_finalized_requests_on_user_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "requests", force: :cascade do |t|
     t.bigint "user_id"
     t.datetime "created_at", null: false
@@ -240,6 +257,8 @@ ActiveRecord::Schema.define(version: 2020_04_08_133209) do
   add_foreign_key "expert_applications", "talent_types"
   add_foreign_key "finalized_requests", "requests"
   add_foreign_key "finalized_requests", "users"
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
   add_foreign_key "requests", "users"
   add_foreign_key "talent_assignments", "expert_applications"
   add_foreign_key "talent_assignments", "finalized_requests"
