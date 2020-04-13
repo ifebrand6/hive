6c20efa1f1cdccc184959f58cac4cdb6daf23744
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-13 20:59:26 +0100

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # resources :blog
   get 'blog'  => 'blog#index'
   get 'blog/single_post/:id' => 'blog#single_post', :as => 'single_post'
-  post 'blog/add_comment' => 'blog#add_comment'
-  # get '/blog/:id', to: 'blog#show'
   resources :posts 
   resources :comments, only: [:create,:show]
   resources :get_experts, only: [:new,:create], path: 'book-an-agriculture-expert_service'
@@ -14,8 +11,8 @@ Rails.application.routes.draw do
   get 'admin/customer_talent_requests/:id' => 'admin#customer_talent_requests', :as => 'talent_requests'
   get 'admin/customer_requests/:id' => 'admin#customer_requests', :as => 'customer_requests'
   get 'admin/talent_assignment/:id' => 'admin#talent_assignment', :as => 'talent_assignment'
-  post "admin/finalize_user_request", to: "admin#finalize_user_request"
-  get 'onboard/:id' => 'expert_applications#accept_application', :as => 'onboard'
+  post "admin/finalize_user_request(/:id)", to: "admin#finalize_user_request"
+  get 'accept/:id' => 'expert_applications#accept_application', :as => 'onboard'
   resources :expert_applications, path: 'application'
   mount RailsAdmin::Engine => '/super_admin', as: 'rails_admin'
   root to: "home#index"
