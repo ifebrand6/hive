4d5bb9c04be00bcc8e8e8440c95d4ef28be28856
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-14 14:24:17 +0100

    add keys

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,11 @@
 # Load the Rails application.
 require_relative 'application'
+require "paystack"
 ADMIN_EMAIL = 'ifebrand6@gmail.com'
 GMAIL_USERNAME = 'ifebrand6@gmail.com'
 GMAIL_PASSWORD = '1997date'
+PAYSTACK_PUBLIC_KEY = Paystack.new
+PAYSTACK_PRIVATE_KEY = Paystack.new
 
 # Initialize the Rails application.
 Rails.application.initialize!
