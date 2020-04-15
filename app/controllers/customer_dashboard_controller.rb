52b61a34862cc114c07e75d769ed637d104c3c56
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-15 11:42:02 +0100

    generated customer dashboard controller

---
+++ app/controllers/customer_dashboard_controller.rb
@@ -0,0 +1,28 @@
+class CustomerDashboardController < ApplicationController
+  def index
+  end
+
+  def finalized_requests
+  end
+
+  def show_finalized_request
+  end
+
+  def make_payment_for_finalized_request
+  end
+
+  def transctions
+  end
+
+  def show_transcation
+  end
+
+  def request
+  end
+
+  def show_request
+  end
+
+  def delete_request
+  end
+end
