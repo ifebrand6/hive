44a7c1a9c2149da0ab70f223d7dc60fe81f2f16b
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-15 18:45:07 +0100

    list the finalized request in Customer Dashboard

--- app/controllers/customer_dashboard_controller.rb
+++ app/controllers/customer_dashboard_controller.rb
@@ -8,9 +8,13 @@ class CustomerDashboardController < ApplicationController
   end
 
   def show_finalized_request
+    @finalized_request = FinalizedRequest.find(params[:id])
+    @talent_assignment = @finalized_request.talent_assignment
   end
 
   def make_payment_for_finalized_request
+    @transaction = Transaction.new
+    @profile = @transaction.build_user
   end
 
   def transactions
