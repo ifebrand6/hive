2181781899685b55297d93eaf3cff0bec9e91b3e
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-19 15:27:58 +0100

    chnage fil name to fix helper error

---
+++ app/controllers/experts_controller.rb
@@ -0,0 +1,41 @@
+class ExpertsController < ApplicationController
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, :only => [:index, :new, :create, :show, :accept_application]
+
+  def index
+    @expert_specialization_list = TalentType.all
+  end
+
+  def new
+    @expert_specialization_list = TalentType.all
+    @expert = Expert.new
+  end
+  def show
+    @expert = Expert.find(params[:id])
+  end
+  
+  def create
+    @expert_specialization_list = TalentType.all
+    @expert = Expert.new(expert_params)
+    if @expert.save
+      redirect_to action: "index", flash: "APPLICATION SUBMITTED SUCCESSFULLY"
+    else
+      render :new
+    end
+  end
+  def accept_application
+    @expert = Expert.find(params[:id])
+    @expert.onboarding_expert
+    redirect_to dashboard_application_path
+  end
+  
+
+
+    private
+    def expert_params
+      params.require(:expert).permit(:firstName,:lastName,:email, :phoneNumber, :contactAddress,:shortBio, :talent_type_id, :suggested_skill)
+    end
+    def talent_type_params
+      params.require(:talent_type).permit(:expert_specialization)
+    end
+end
