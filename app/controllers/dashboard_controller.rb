39911cd6592d1f9459986dbda0111ac47813299e
udeogu ifechuwku <ifebrand6@gmail.com>
2020-04-25 16:54:15 +0100

    add link to mailer

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,23 +11,28 @@ class DashboardController < ApplicationController
   def talents
     @expert_list = Expert.all.onboard_expert
   end
+
   def customers_requests
     @customers = User.customers
   end
+
   def customer_requests
     @user = User.find(params[:id])
     @requests = @user.requests
   end
+
   def customer_talent_requests
     @request = Request.find(params[:id])
     @talent_requests =  @request.talent_requests
   end
+
   def talent_assignment
     @talent_request = TalentRequest.find(params[:id]) #use to get the TalentRequest and populated to fill in the final request params
     @talents = Expert.all.where(talent_type: @talent_request.talent_type_id)
     @finalized_request = FinalizedRequest.new
     @finalized_request.build_talent_assignment
   end
+  
   def finalize_user_request
       @finalized_request = FinalizedRequest.new(final_params)
       if @finalized_request.save!
